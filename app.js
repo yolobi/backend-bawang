3ef8c36f9dff0e2ecd6940f82528de92f5f247d6
Aqshal Deandra Vandaru <aqshal.deandra@gmail.com>
2022-03-30 13:47:45 +0700

    install admin-lte

--- app.js
+++ app.js
@@ -17,16 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/adminlte',
+  express.static(path.join(__dirname, '/node_modules/admin-lte/'))
+);
 
 app.use('/', blankoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
