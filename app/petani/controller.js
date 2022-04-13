8f89366ebe05257b46f2d47c66aa1241a3fb9111
Aqshal Deandra Vandaru <aqshal.deandra@gmail.com>
2022-04-13 16:58:18 +0700

    initial commit

--- app/petani/controller.js
+++ app/petani/controller.js
@@ -2,7 +2,7 @@ module.exports = {
   index: async (req, res) => {
       console.log('berhasil masuk')
       res.status(200).json({
-          message: 'berhasil masuk'
+          message: 'berhasil masuk, Selamat Datang!'
       })
   },
 };
