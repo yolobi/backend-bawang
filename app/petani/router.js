b2e2cb8f418ac374eb283594ef32068bf63e477a
Aqshal Deandra Vandaru <aqshal.deandra@gmail.com>
2022-04-13 15:51:06 +0700

    initial commit

--- app/petani/router.js
+++ app/petani/router.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const { index } = require('./controller');
 const authenticateUser = require('../middleware/authentication');
-const {checkIfPetani} = require('../middleware/check-role');
+const { checkIfPetani } = require('../middleware/check-role');
 
 /* GET home page. */
-router.get('/', authenticateUser, checkIfPetani, index);
+router.get('/', index);
 
 module.exports = router;
