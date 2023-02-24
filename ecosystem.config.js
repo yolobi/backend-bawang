abc95a6081b61062d762d4535218091721a5614b
yolobi <dimdimdimas@apps.ipb.ac.id>
2023-02-24 22:16:19 +0700

    setup

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'dimnab',
+      host : 'gcp-bawang',
+      ref  : 'origin/master',
+      repo : 'https://github.com/yolobi/backend-bawang.git',
+      path : '/home/dimnab/backend-bawang',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
