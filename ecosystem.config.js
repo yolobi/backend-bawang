328f1dc1efbf103c0716b5a025ec750aa8765b0d
yolobi <dimdimdimas@apps.ipb.ac.id>
2023-07-06 07:39:47 +0700

    config pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,11 @@
-module.exports = {
-  apps : [{
-    script: 'bin/www',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+const { watch } = require('./app/blanko2/model');
 
-  deploy : {
-    production : {
-      user : 'dimnab',
-      host : 'gcp-bawang',
-      ref  : 'origin/master',
-      repo : 'https://github.com/yolobi/backend-bawang.git',
-      path : '/home/dimnab/backend-bawang',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+module.exports = {
+  apps: [
+    {
+      name: 'app1',
+      script: './app.js',
+      watch: false,
+    },
+  ],
 };
